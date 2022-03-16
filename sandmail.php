cf6b9f7231b8d95ed8339a46e9b9a99b9736fb4d
zahernaher <74210661+zahernaher@users.noreply.github.com>
2022-03-16 22:42:28 +0300

    Project pre-end

--- sandmail.php
+++ sandmail.php
@@ -15,8 +15,63 @@
 	"X-Mailer: PHP/" . phpversion();
 
 	if(mail($to, $name, $message, $headers)) {
-		echo 'Письмо отправлено';		
+		echo "<!DOCTYPE html>
+		<html lang='en'>
+		<head>
+		<meta charset='UTF-8'>
+		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+		<title>Document</title>
+		<style>
+		
+				@import 'fonts.css';
+		
+				*{			
+					color: rgb(219, 219, 219);
+					font-family: 'DINPro';
+				}
+		
+				h1{
+					text-align: center;
+				}
+		</style>
+</head>
+<body>
+<h1>Письмо отправлено</h1>
+	<script src='./iframeResizer.contentWindow.min.js'>		
+	</script> 
+</body>
+</html>";
+			
 	} else {
-		echo 'Письмо не отправлено';
+		echo "<!DOCTYPE html>
+		<html lang='en'>
+		<head>
+		<meta charset='UTF-8'>
+		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+		<title>Document</title>
+		<style>
+		
+				@import 'fonts.css';
+		
+				*{			
+					color: rgb(219, 219, 219);
+					font-family: 'DINPro';
+				}
+		
+				h1{
+					text-align: center;
+				}
+		</style>
+</head>
+<body>
+<h1>Письмо не отправлено</h1>
+	<script src='./iframeResizer.contentWindow.min.js'>		
+	</script> 
+</body>
+</html>";
 	}
+
+	
 ?>
