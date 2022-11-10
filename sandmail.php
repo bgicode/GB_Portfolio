6232437125703ae46b03390427e473bef641f271
zahernaher <74210661+zahernaher@users.noreply.github.com>
2022-11-10 20:30:49 +0300

    enable_mailer

--- sandmail.php
+++ sandmail.php
@@ -1,77 +1,84 @@
 <?php
-	$to = "bgdesigner@yandex.ru";
-	$from = trim($_POST['email']);
+  require 'phpmailer/PHPMailer.php';
+	require 'phpmailer/SMTP.php';
+	require 'phpmailer/Exception.php';
 
-	$name = htmlspecialchars($_POST['name']);
-	$name = urldecode($name);
-	$name = trim($name);
+	$name = $_POST['user_name'];
+	$email = $_POST['user_email'];
+	$text = $_POST['message'];
 
-	$message = htmlspecialchars($_POST['message']);
-	$message = urldecode($message);
-	$message = trim($message);
+	$title = "Письмо с порфолио";
+	$body = "
+	<h2>Новое письмо</h2>
+	<b>Имя:</b> $name<br>
+	<b>Почта:</b> $email<br><br>
+	<b>Сообщение:</b><br>$text";
+	
+	$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
-	$headers = "From: $from" . "\r\n" . 
-	"Reply-To: $from" . "\r\n" . 
-	"X-Mailer: PHP/" . phpversion();
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'desimo123@mail.ru'; // Логин на почте
+    $mail->Password   = 'Hi7t9u1EjjXipb05dRtV'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('desimo123@mail.ru', 'Имяотправителя'); // Адрес самой почты и имя отправителя
 
-	if(mail($to, $name, $message, $headers)) {
-		echo "<!DOCTYPE html>
-		<html lang='en'>
-		<head>
-		<meta charset='UTF-8'>
-		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
-		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
-		<title>Document</title>
-		<style>
-		
-				@import 'fonts.css';
-		
-				*{			
-					color: rgb(219, 219, 219);
-					font-family: 'DINPro';
-				}
-		
-				h1{
-					text-align: center;
-				}
-		</style>
+    // Получатель письма
+    $mail->addAddress('bgdesigner@yandex.ru');  
+    
+
+    
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "Сообщение отправлено";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+echo "<!DOCTYPE html>
+<html lang='en'>
+<head>
+<meta charset='UTF-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<title>Document</title>
+<style>
+
+		@import 'fonts.css';
+
+		*{			
+			color: rgb(219, 219, 219);
+			font-family: 'DINPro';
+		}
+
+		h1{
+			text-align: center;
+		}
+</style>
 </head>
 <body>
-<h1>Письмо отправлено</h1>
-	<script src='./iframeResizer.contentWindow.min.js'>		
-	</script> 
+<h1>"
+?> 
+<?php echo implode(["result" => $result, "status" => $status]);"</h1>
+<script src='./iframeResizer.contentWindow.min.js'>		
+</script> 
 </body>
-</html>";
-			
-	} else {
-		echo "<!DOCTYPE html>
-		<html lang='en'>
-		<head>
-		<meta charset='UTF-8'>
-		<meta http-equiv='X-UA-Compatible' content='IE=edge'>
-		<meta name='viewport' content='width=device-width, initial-scale=1.0'>
-		<title>Document</title>
-		<style>
-		
-				@import 'fonts.css';
-		
-				*{			
-					color: rgb(219, 219, 219);
-					font-family: 'DINPro';
-				}
-		
-				h1{
-					text-align: center;
-				}
-		</style>
-</head>
-<body>
-<h1>Письмо не отправлено</h1>
-	<script src='./iframeResizer.contentWindow.min.js'>		
-	</script> 
-</body>
-</html>";
-	}
+</html>"
 
 	
 ?>
